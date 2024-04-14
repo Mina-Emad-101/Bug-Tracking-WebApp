0756141ae8711295d89ae501937c0fc0439e287a
Mina-Emad-101 <menamanos@gmail.com>
2024-04-14 18:38:32 +0200

    Added functionality for permissions of Customers and Staff

---
+++ Controllers/customerPermissions.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__.'/../Models/user.php';
+session_start();
+if(!isset($_SESSION['loggedInUser']))
+	header('location:http://'.$_SERVER['SERVER_NAME'].'/BugTrackingApplication/Views/html/auth/authentication-login.php');
+elseif($_SESSION['loggedInUser']->getRole() != 'Customer')
+	header($_SESSION['main']);
+?>
--- Views/html/Customer/main.php
+++ Views/html/Customer/main.php
@@ -1,3 +1,6 @@
+<?php
+require_once __DIR__.'/../../../Controllers/customerPermissions.php';
+?>
 <!doctype html>
 <html lang="en">
 
@@ -14,110 +17,12 @@
 		<div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
 			data-sidebar-position="fixed" data-header-position="fixed">
 			<!-- Sidebar Start -->
-			<aside class="left-sidebar">
-				<!-- Sidebar scroll-->
-				<div>
-					<div class="brand-logo d-flex align-items-center justify-content-between">
-						<a href="./dashboard.php" class="text-nowrap logo-img">
-							<img src="../../assets/images/logos/dark-logo.svg" width="180" alt="" />
-						</a>
-						<div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-							<i class="ti ti-x fs-8"></i>
-						</div>
-					</div>
-					<!-- Sidebar navigation-->
-					<nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-						<ul id="sidebarnav">
-							<li class="nav-small-cap">
-								<i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-								<span class="hide-menu">Home</span>
-							</li>
-							<li class="sidebar-item">
-								<a class="sidebar-link" href="./dashboard.php" aria-expanded="false">
-									<span>
-										<i class="ti ti-layout-dashboard"></i>
-									</span>
-									<span class="hide-menu">Dashboard</span>
-								</a>
-							</li>
-							<li class="nav-small-cap">
-								<i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-								<span class="hide-menu">UI COMPONENTS</span>
-							</li>
-							<li class="sidebar-item">
-								<a class="sidebar-link" href="./ui-buttons.php" aria-expanded="false">
-									<span>
-										<i class="ti ti-article"></i>
-									</span>
-									<span class="hide-menu">Buttons</span>
-								</a>
-							</li>
-							<li class="nav-small-cap">
-								<i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-								<span class="hide-menu">AUTH</span>
-							</li>
-							<li class="sidebar-item">
-								<a class="sidebar-link" href="./authentication-login.php" aria-expanded="false">
-									<span>
-										<i class="ti ti-login"></i>
-									</span>
-									<span class="hide-menu">Login</span>
-								</a>
-							</li>
-						</ul>
-					</nav>
-					<!-- End Sidebar navigation -->
-				</div>
-				<!-- End Sidebar scroll-->
-			</aside>
+			<?php require './basics/sidebar.php'; ?>
 			<!--  Sidebar End -->
 			<!--  Main wrapper -->
 			<div class="body-wrapper">
 				<!--  Header Start -->
-				<header class="app-header">
-					<nav class="navbar navbar-expand-lg navbar-light">
-						<ul class="navbar-nav">
-							<li class="nav-item d-block d-xl-none">
-								<a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse" href="javascript:void(0)">
-									<i class="ti ti-menu-2"></i>
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link nav-icon-hover" href="javascript:void(0)">
-									<i class="ti ti-bell-ringing"></i>
-									<div class="notification bg-primary rounded-circle"></div>
-								</a>
-							</li>
-						</ul>
-						<div class="navbar-collapse justify-content-end px-0" id="navbarNav">
-							<ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-								<li class="nav-item dropdown">
-									<a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
-										aria-expanded="false">
-										<img src="../../assets/images/profile/user-1.jpg" alt="" width="35" height="35" class="rounded-circle">
-									</a>
-									<div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
-										<div class="message-body">
-											<a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-												<i class="ti ti-user fs-6"></i>
-												<p class="mb-0 fs-3">My Profile</p>
-											</a>
-											<a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-												<i class="ti ti-mail fs-6"></i>
-												<p class="mb-0 fs-3">My Account</p>
-											</a>
-											<a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-												<i class="ti ti-list-check fs-6"></i>
-												<p class="mb-0 fs-3">My Task</p>
-											</a>
-											<a href="../auth/logout.php" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
-										</div>
-									</div>
-								</li>
-							</ul>
-						</div>
-					</nav>
-				</header>
+				<?php require './basics/header.php'; ?>
 				<!--  Header End -->
 				<div class="container-fluid">
 					<div class="card">
